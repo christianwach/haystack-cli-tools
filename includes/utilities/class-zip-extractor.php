7f4c3183424d46abd769e0d39420b3811f9dd349
Christian Wach <needle@haystack.co.uk>
2026-02-06 13:58:34 +0000

    Add missing class files

---
+++ includes/utilities/class-zip-extractor.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Zip extractor class.
+ *
+ * @package Haystack_Command_Line_Tools
+ */
+
+// Make sure WP_Upgrader exists.
+if ( ! class_exists( 'WP_Upgrader' ) ) {
+	require_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
+}
+
+/**
+ * Zip extractor class.
+ *
+ * @since 1.0.0
+ */
+class CLI_Tools_CiviCRM_Zip_Extractor extends WP_Upgrader {
+
+	/**
+	 * Whether a bulk upgrade/installation is being performed.
+	 *
+	 * @since 1.0.0
+	 * @access public
+	 * @var bool
+	 */
+	public $bulk = false;
+
+	/**
+	 * Initializes the extract strings.
+	 *
+	 * @since 1.0.0
+	 */
+	public function extract_strings() {
+		$this->strings['unpack_package']     = 'Unpacking the archive...';
+		$this->strings['installing_package'] = 'Installing the archive...';
+		$this->strings['remove_old']         = 'Removing the existing directory...';
+		$this->strings['remove_old_failed']  = 'Could not remove the existing directory.';
+		$this->strings['process_failed']     = 'Extraction failed.';
+		$this->strings['process_success']    = 'Extraction completed successfully.';
+	}
+
+	/**
+	 * Extracts a zip archive to a directory.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $zipfile The path to the zipfile.
+	 * @param string $destination The directory name to extract to.
+	 * @param array  $settings The array of extraction settings.
+	 * @return array|false|WP_Error The result on success, otherwise a WP_Error, or false if unable to connect to the filesystem.
+	 */
+	public function extract( $zipfile, $destination, $settings ) {
+
+		$this->extract_strings();
+
+		$options = [
+			'package'     => $zipfile,
+			'destination' => untrailingslashit( $destination ),
+		];
+
+		$defaults = [
+			'clear_destination'           => true,
+			'clear_working'               => true,
+			'abort_if_destination_exists' => false,
+		];
+
+		$settings = wp_parse_args( $settings, $defaults );
+
+		$options = $options + $settings;
+
+		return $this->run( $options );
+
+	}
+
+}
